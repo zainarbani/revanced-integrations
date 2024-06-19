82700dd7472644fdb9c595c6638df4b2726517a9
zainarbani <zaintsyariev@gmail.com>
2024-06-19 13:44:59 +0700

    fix(yt): aaa4

--- app/src/main/java/app/revanced/integrations/youtube/patches/spoof/SpoofClientPatch.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/spoof/SpoofClientPatch.java
@@ -5,6 +5,7 @@ import android.media.MediaCodecList;
 import android.net.Uri;
 import android.os.Build;
 import app.revanced.integrations.shared.Logger;
+import app.revanced.integrations.shared.Utils;
 import app.revanced.integrations.youtube.settings.Settings;
 
 @SuppressWarnings("unused")
@@ -103,6 +104,21 @@ public class SpoofClientPatch {
     /**
      * Injection point.
      */
+    public static String getUserAgent(String originalUserAgent) {
+        if (SPOOF_CLIENT_ENABLED && SPOOF_CLIENT_TYPE == ClientType.IOS) {
+            String iosUserAgent = "com.google.ios.youtube/"
+                    + Utils.getAppVersionName()
+                    + " (iPhone; U; CPU iPhone OS 17_5_1 like Mac OS X) gzip";
+            
+            return iosUserAgent;
+        }
+
+        return originalUserAgent;
+    }
+    
+    /**
+     * Injection point.
+     */
     public static boolean enablePlayerGesture(boolean original) {
         return SPOOF_CLIENT_ENABLED || original;
     }
@@ -129,7 +145,7 @@ public class SpoofClientPatch {
 
     private enum ClientType {
         // https://dumps.tadiphone.dev/dumps/oculus/eureka
-        ANDROID_VR(2, "Pixel 7 Pro", "2.20240614.01.00"),
+        ANDROID_VR(28, "Quest 3", "1.56.21"),
         // 11,4 = iPhone XS Max.
         // 16,2 = iPhone 15 Pro Max.
         // Since the 15 supports AV1 hardware decoding, only spoof that device if this
@@ -167,7 +183,10 @@ public class SpoofClientPatch {
             MediaCodecList codecList = new MediaCodecList(MediaCodecList.ALL_CODECS);
 
             for (MediaCodecInfo codecInfo : codecList.getCodecInfos()) {
-                if (codecInfo.isHardwareAccelerated() && !codecInfo.isEncoder()) {
+                // Allow AV1 software decoder on Android S.
+                // https://www.threads.net/@mishaal_rahman/post/C56-DqnLPhN
+                boolean isAV1Capable = (codecInfo.isHardwareAccelerated() || Build.VERSION.SDK_INT >= Build.VERSION_CODES.S);
+                if (isAV1Capable && !codecInfo.isEncoder()) {
                     String[] supportedTypes = codecInfo.getSupportedTypes();
                     for (String type : supportedTypes) {
                         if (type.equalsIgnoreCase("video/av01")) {
